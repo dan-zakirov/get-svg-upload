046825ffff30d4d221b6fd774f23183f4681a5b5
Dan Zakirov <41396370+dan-zakirov@users.noreply.github.com>
2024-09-24 11:03:02 +0300

    UPDATE: Исправлена обработка тега <svg> для учета регистра
    # Использован preg_replace_callback для добавления класса к тегу <svg>
    # Обработка тегов теперь игнорирует регистр, позволяя использовать любые вариации написания

--- functions.php
+++ functions.php
@@ -22,7 +22,15 @@ function air_get_svg_with_class( $attachment_id ) {
         return __( 'SVG file not found.', 'your-text-domain' );
     }
 
-    $svg_content = preg_replace('/<svg([^>]*)/', '<svg$1 class="upload"', file_get_contents($file_path));
-    
+    $svg_content = file_get_contents($file_path);
+
+    $svg_content = preg_replace_callback(
+        '/<svg([^>]*)/', 
+        function($matches) {
+            return '<svg' . $matches[1] . ' class="upload"';
+        },
+        $svg_content
+    );
+
     return $svg_content;
 }
